8026fd7aba7d563fcfbe6a186fa2ad993c12d3db
ShawonMridha <you@example.com>
2021-09-13 15:18:05 +0600

    my debug sevnth assignmet

--- js/app.js
+++ js/app.js
@@ -8,16 +8,26 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
+    console.log(product);
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `<div class="single-product bg-secondary">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
+      <p>${product.rating.rate}</p>
+      <div class="text-warning">
+      <i class="fas fa-star filled"></i>
+      <i class="fas fa-star filled"></i>
+      <i class="fas fa-star filled"></i>
+      <i class="fas fa-star filled"></i>
+      </div>
+      <p>${product.rating.count} ratings</p> 
+      
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
       `;
@@ -35,7 +45,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
